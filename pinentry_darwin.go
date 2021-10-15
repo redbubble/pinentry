5e927fa7c3b8d4d35f808e72f3bc197ae208a520
paul <423357+toothbrush@users.noreply.github.com>
2021-10-15 12:24:17 +1100

    pinentry_darwin.go: Force pinentry-mac.

--- pinentry_darwin.go
+++ pinentry_darwin.go
@@ -6,8 +6,5 @@ import "github.com/gopasspw/pinentry/gpgconf"
 
 // GetBinary always returns pinentry-mac
 func GetBinary() string {
-	if p, err := gpgconf.Path("pinentry"); err == nil && p != "" {
-		return p
-	}
 	return "pinentry-mac"
 }
